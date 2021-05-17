f38e2e02bff4b37ae4ac0be0e7c6962f852dc058
Dirk-Jan Wassink <dirk.jan.wassink@gmail.com>
2021-05-17 21:52:43 +0200

    Simplified telegram unread count

--- service-recipes/webz-telegram.js
+++ service-recipes/webz-telegram.js
@@ -1,13 +1,7 @@
 const { ipcRenderer } = require("electron");
 
 setInterval(() => {
-  const badges = Array.from(
-    document.querySelectorAll(".active .chat-list .ListItem.Chat .title")
-  )
-    .filter((title) => !title.querySelector(".icon-muted-chat"))
-    .map((title) => title.closest(".ListItem"))
-    .map((chatItem) => chatItem.querySelector(".Badge.unread"))
-    .filter((badge) => badge != null);
+  const badges = document.querySelectorAll(".Badge.unread:not(.muted)");
   ipcRenderer.sendToHost("", badges.length);
   ipcRenderer.send("count", "webz-telegram", badges.length);
 }, 1000);
